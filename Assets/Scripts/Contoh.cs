f8771d4f649c92fd18fc816341d6a29fc15999c3
dimassetyoo <dimassetyoo@gmail.com>
2025-11-10 17:05:49 +0700

    Belajar Fundamental Programming C#

--- Assets/Scripts/Contoh.cs
+++ Assets/Scripts/Contoh.cs
@@ -1,16 +1,44 @@
+using System.Collections;
+using JetBrains.Annotations;
 using UnityEngine;
+using UnityEngine.Timeline;
 
 public class Contoh : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        /*  int Number1 = 10;
+         int Number2 = 7;
+         int temp = Number1;
+         Number1 = Number2;
+         Number2 = temp;
+         Debug.Log(Number2);
+         Debug.Log(Number1); */
+        /* int number = 3;
+         switch (number)
+         {
+             case 1:
+                 Debug.Log("Basic Sword");
+                 break;
+             case 2:
+                 Debug.Log("Electric Bow");
+                 break;
+             case 3:
+                 Debug.Log("Fire Spell");
+                 break;
+         } */
+        Debug.Log(kali(5, 10, 5));
+         
     }
-
+    public int kali( int angka1, int angka2, int angka3)
+    {
+        int result = angka1 * angka2 * angka3;
+        return result;
+    }
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
 }
--- Assets/Scripts/contoh2.cs
+++ Assets/Scripts/contoh2.cs
@@ -5,7 +5,16 @@ public class contoh2 : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        int age = 17;
+        int money = 50000;
+        if (age >= 18 || money >= 100000)
+        {
+            Debug.Log("dapat masuk");
+        }
+        else
+        {
+            Debug.Log("tidak bisa masuk");
+        }
     }
 
     // Update is called once per frame
