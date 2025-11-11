5ab7223a5eca1e670ab9864df147c5fbfea762df
dimassetyoo <dimassetyoo@gmail.com>
2025-11-11 12:17:59 +0700

    Struktur data

---
+++ Assets/Scripts/Day2.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class Day2 : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // while loop
+        /*int nyawa = 10;
+        while (nyawa > 0)
+        {
+            nyawa = nyawa - 1;
+            Debug.Log(nyawa);
+        }
+        Debug.Log("Game Over"); */
+
+        //For loop
+        /*int angka = 4;
+        int faktor = 1;
+        for (int i = angka; i >= 1; i--)
+          {
+            faktor *= i;
+            Debug.Log("pengulangan ke-" + i);
+            
+          }
+       Debug.Log("jumlahnya adalah" + faktor)*/
+    }
+    // Update is called once per frame
+    
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Day2_strukturdata.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class Day2_strukturdata : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField]
+    Player player;
+    void Start()
+    {
+        /* Player player = new Player();
+         player.Name = "Dimas";
+         player.level = 1;
+         player.health = 100;
+
+         Player player2 = new Player();
+         player2.Name = "Dimas";
+         player2.level = 2;
+         player2.health = 500;
+         */
+        /*Player player = new Player("dimas", 1, 100);
+        Player player2 = new Player("dims", 2, 200);
+
+        Debug.Log(player.Name);
+        Debug.Log(player.Level);
+        Debug.Log(player.Health);
+
+        Debug.Log(player2.Name);
+        Debug.Log(player2.Level);
+        Debug.Log(player2.Health); */
+
+        /* Debug.Log(player.Name);
+        Debug.Log(player.Level);
+        Debug.Log(player.Health); */
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
