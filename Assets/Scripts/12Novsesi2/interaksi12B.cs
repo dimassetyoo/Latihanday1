cb3d6c342f8c30b858c8f6b49336137d489d3f72
dimassetyoo <dimassetyoo@gmail.com>
2025-11-13 09:26:07 +0700

    adding comment

--- Assets/Scripts/12Novsesi2/interaksi12B.cs
+++ Assets/Scripts/12Novsesi2/interaksi12B.cs
@@ -4,8 +4,14 @@ using TMPro;
 public class interaksi12B : MonoBehaviour
 {
     public SquareMove12B squareMove12B;
+    // digunakan sebagai referensi ke class lain bernama SquareMove12B
+    // Tujuannya agar script ini bisa berkomunikasi dengan script SquareMove12B di objek lain
     public int skor;
+    // Variabel public dengan tipe data int dengan nama skor untuk menyimpan jumlah skor pemain
     public TMP_Text TeksSkor;
+    // Referensi ke komponen teks TMP_Text (TextMeshPro)
+    // Digunakan untuk menampilkan nilai skor di layar
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
